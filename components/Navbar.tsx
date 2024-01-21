864ff595b8cc7240704c658a2035ad2bd3528bdd
Huỳnh Quang Bảo <hbao592000@gmail.com>
2024-01-21 11:21:06 +0700

    feat: navbar-component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,12 @@ import Link from "next/link";
 
 import Container from "@/components/ui/Container";
 import MainNav from "@/components/main-nav";
+import getCategories from "@/actions/get-categories";
+import NavbarActions from "@/components/navbar-actions";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const categories = await getCategories();
+
   return (
     <div className="border-b">
       <Container>
@@ -12,7 +16,8 @@ const Navbar = () => {
           <Link href="/" className="flex ml-4 lg:ml-0 gap-x-2">
             <p className="text-xl font-bold uppercase">Store</p>
           </Link>
-          <MainNav data={[]} />
+          <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
