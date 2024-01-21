997fa1afb11a8b77f46259f3c574b552ddc2c15b
Huỳnh Quang Bảo <hbao592000@gmail.com>
2024-01-21 10:40:10 +0700

    feat: setup-env-file

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,10 +1,14 @@
+"use client"
+
 import React from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+
 import { cn } from "@/lib/utils";
+import { Category } from "@/types";
 
 interface MainNavProps {
-  data: any;
+  data: Category[];
 }
 
 const MainNav: React.FC<MainNavProps> = ({ data }) => {
